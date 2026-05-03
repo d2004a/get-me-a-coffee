cd5863167adf7c28ade00885d799e9391094769d
Divyanshu agarwal <agarwaldivyanshu824@gmail.com>
2026-05-03 21:15:38 +0530

    Database issue resolved

--- db/connectDb.js
+++ db/connectDb.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 const connectDb = async () => {
         try {
-            const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/coffee', {
+            const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/coffee', {
                 useNewUrlParser: true,
             });
             console.log(`MongoDB Connected: ${conn.connection.host}`);
